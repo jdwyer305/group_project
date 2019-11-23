80dfe572934ae7d5872cfde8e586a2dff5bc4096
vargaskenneth27 <57016938+vargaskenneth27@users.noreply.github.com>
2019-11-23 00:31:12 -0500

    Update javascript_.js

--- javascript_.js
+++ javascript_.js
@@ -27,20 +27,20 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-$(document.getElementById("#prev")).keypress(function(e) {
+//$(document.getElementById("#prev")).keypress(function(e) {
   //check if enter is pressed
-  if (e.keyCode == 13) {
+  //if (e.keyCode == 13) {
     //click your link
-    $(document.getElementsByClassName("#prev").click();
-  }
-  });
-$(document.getElementById("#next")).keypress(function(e) {
+    //$(document.getElementsByClassName("#prev").click();
+  //}
+  //});
+//$(document.getElementById("#next")).keypress(function(e) {
   //check if enter is pressed
-  if (e.keyCode == 13) {
+  //if (e.keyCode == 13) {
     //click your link
-    $(document.getElementsByClassName("#next").click();
-  }
-  });
+    //$(document.getElementsByClassName("#next").click();
+  //}
+  //});
 
 $(document.getElementsByClassName("dot")[1]).keypress(function(e) {
   //check if enter is pressed
