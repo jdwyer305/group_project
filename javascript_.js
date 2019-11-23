7124b5e0f4bc00917a4fd6fd2e11f5a1e4262dc4
vargaskenneth27 <57016938+vargaskenneth27@users.noreply.github.com>
2019-11-22 23:56:33 -0500

    Update javascript_.js

--- javascript_.js
+++ javascript_.js
@@ -27,6 +27,45 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+$(document.getElementById("#prev").keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("#prev").click();
+  }
+  });
+$(document.getElementById("#next").keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("#next").click();
+  }
+  });
+
+$(document.getElementsByClassName("dot")[0]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[0]).click();
+  }
+  });
+
+$(document.getElementsByClassName("dot")[1]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[1]).click();
+  }
+  });
+
+$(document.getElementsByClassName("dot")[2]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[2]).click();
+  }
+  });
+   
 $(document.getElementsByClassName("dot")[3]).keypress(function(e) {
   //check if enter is pressed
   if (e.keyCode == 13) {
@@ -34,5 +73,110 @@ $(document.getElementsByClassName("dot")[3]).keypress(function(e) {
     $(document.getElementsByClassName("dot")[3]).click();
   }
   });
-
+ 
+ $(document.getElementsByClassName("dot")[4]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[4]).click();
+  }
+  });
+ 
+ $(document.getElementsByClassName("dot")[5]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[5]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[6]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[6]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[7]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[7]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[8]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[8]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[9]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[9]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[10]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[10]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[11]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[11]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[12]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[12]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[13]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[13]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[14]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[14]).click();
+  }
+  });
+    
+ $(document.getElementsByClassName("dot")[15]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[15]).click();
+  }
+  });
+    
+  $(document.getElementsByClassName("dot")[16]).keypress(function(e) {
+  //check if enter is pressed
+  if (e.keyCode == 13) {
+    //click your link
+    $(document.getElementsByClassName("dot")[16]).click();
+  }
+  });
+    
+ 
     
